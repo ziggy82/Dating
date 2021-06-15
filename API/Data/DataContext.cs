4080f3de14cc48d3630bdcf2df22247e8744ec18
ziggy82 <zaheir786@Hotmail.com>
2021-06-15 17:21:04 -0400

    Initial check-in of module DatingApp

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
